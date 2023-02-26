0067510558c3ba95297ed6d24014261e53c9473b
dg1223 <alavi1223@hotmail.com>
2023-02-26 07:13:56 -0500

    JS errors

---
+++ js/error.js
@@ -0,0 +1,9 @@
+// fetch("https://jsonplaceholder.typicode.com/users")
+//   .then((res) => res.json())
+//   .then((data) => console.log(data));
+
+console.log(1);
+setTimeout(() => {
+  console.log(2);
+}, 3000);
+console.log(3);
