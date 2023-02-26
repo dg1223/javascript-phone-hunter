9542f30872bd5a6aeea42857d75e27f0db60e099
dg1223 <alavi1223@hotmail.com>
2023-02-26 04:49:37 -0500

    spinner

--- js/app.js
+++ js/app.js
@@ -35,10 +35,24 @@ const displayPhones = (phones) => {
     `;
     phoneContainer.appendChild(phoneDiv);
   });
+  // stop spinner or loader
+  toggleSpinner(false);
 };
 
+// handle search button click
 document.getElementById("btn-search").addEventListener("click", function () {
+  // start loader
+  toggleSpinner(true);
   const searchField = document.getElementById("search-field");
   const searchText = searchField.value;
   loadPhones(searchText);
 });
+
+const toggleSpinner = (isLoading) => {
+  const loaderSection = document.getElementById("loader");
+  if (isLoading) {
+    loaderSection.classList.remove("d-none");
+  } else {
+    loaderSection.classList.add("d-none");
+  }
+};
