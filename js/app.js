1db8222bf9b9f1a80fc2744446724869d2a23a41
dg1223 <alavi1223@hotmail.com>
2023-02-26 06:42:01 -0500

    modal complete

--- js/app.js
+++ js/app.js
@@ -36,7 +36,7 @@ const displayPhones = (phones, dataLimit) => {
             <div class="card-body">
                 <h5 class="card-title">${phone.phone_name}</h5>
                 <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-                <button onclick="loadPhoneDetails('${phone.slug}')" href="#" class="btn btn-primary">Show details</button>
+                <button onclick="loadPhoneDetails('${phone.slug}')" href="#" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#phoneDetailModal">Show details</button>
             </div>
         </div>
     `;
@@ -87,5 +87,25 @@ const loadPhoneDetails = async (id) => {
   const url = `https://openapi.programming-hero.com/api/phone/${id}`;
   const res = await fetch(url);
   const data = await res.json();
-  console.log(data.data);
+  displayPhoneDetails(data.data);
 };
+
+const displayPhoneDetails = (phone) => {
+  console.log(phone);
+  const modalTitle = document.getElementById("phoneDetailModalLabel");
+  modalTitle.innerText = phone.name;
+  const phoneDetails = document.getElementById("phone-details");
+  phoneDetails.innerHTML = `
+    <p>Release date: ${
+      phone.releaseDate ? phone.releaseDate : "No release date found"
+    }</p>
+    <p>Storage: ${
+      phone.mainFeatures ? phone.mainFeatures.storage : "No storage info found"
+    }</p>
+    <p>Others: ${
+      phone.others ? phone.others.Bluetooth : "No additional detials available"
+    }</p>
+  `;
+};
+
+loadPhones("apple");
